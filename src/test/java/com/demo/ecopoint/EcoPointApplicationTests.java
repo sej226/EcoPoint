68340e5cd80c33ee73ebee2fb4a0f3787899b5ee
shin <sej96226@gmail.com>
2022-09-03 21:00:45 +0900

    commit

--- src/test/java/com/demo/ecopoint/EcoPointApplicationTests.java
+++ src/test/java/com/demo/ecopoint/EcoPointApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecopointstandard.demo;
+package com.demo.ecopoint;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EcoPointApplicationTests {
 
 	@Test
 	void contextLoads() {
